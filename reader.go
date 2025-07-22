6a7f47e6ba6d32ab576d312241ff41fe91cea0e4
thisisaaronland <thisisaaronland@localhost>
2025-07-22 15:37:41 -0700

    add reader.go

---
+++ reader.go
@@ -0,0 +1,3 @@
+package fetch
+
+const WHOSONFIRST_DATA_READER_URI string = "findingaid://https/data.whosonfirst.org/findingaid?template=https://raw.githubusercontent.com/whosonfirst-data/{repo}/master/data/"
